03532dbfe1ad0ca35a478ad1b82860e13315fd49
cacobribeiro <cacobribeiro@gmail.com>
2020-05-10 13:37:18 -0300

    Finally test Product Detail and codeclimate are ok!

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,10 +36,14 @@ describe('#productDetails', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.equal(typeof productDetails(), []);
+    assert.equal(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.equal(productDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.equal(typeof productDetails()[0,1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1])
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.equal(productDetails()[0].details.productId.includes(123) && productDetails()[0].details.productId.includes(123), true);
   });
 });
